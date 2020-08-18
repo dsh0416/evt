a4cdd769727c62fc1a74531276802d46fbb48b94
Delton Ding <dsh0416@gmail.com>
2020-08-18 18:33:30 +0900

    Skip multi assignment to consts

--- lib/evt/io.rb
+++ lib/evt/io.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-# Patch for 2.7.1 when tests
-class IO
-  WAIT_READABLE = 1
-  WAIT_WRITABLE = 3
+# Patch for 2.7.1 when testing
+if RUBY_VERSION.start_with?('2.7')
+  class IO
+    WAIT_READABLE = 1
+    WAIT_WRITABLE = 3
+  end
 end
