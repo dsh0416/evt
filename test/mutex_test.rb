eb2cf4138b24b63f5196161b3934fb1c5cb95d6b
Delton Ding <dsh0416@gmail.com>
2020-12-22 23:05:09 +0900

    Add more mutex tests

--- test/mutex_test.rb
+++ test/mutex_test.rb
@@ -45,4 +45,95 @@ class TestMutex < Minitest::Test
     assert_operator secs, :>=, 2
     assert_operator secs, :<, 2.5
   end
+
+  def test_condition_variable
+    mutex = Mutex.new
+    condition = ConditionVariable.new
+
+    signalled = 0
+
+    Thread.new do
+      scheduler = Evt::Scheduler.new
+      Fiber.set_scheduler scheduler
+
+      Fiber.schedule do
+        mutex.synchronize do
+          3.times do
+            condition.wait(mutex)
+            signalled += 1
+          end
+        end
+      end
+
+      Fiber.schedule do
+        3.times do
+          mutex.synchronize do
+            condition.signal
+          end
+
+          sleep 0.1
+        end
+      end
+
+      scheduler.run
+    end.join
+
+    assert_equal 3, signalled
+  end
+
+  def test_queue
+    queue = Queue.new
+    processed = 0
+
+    thread = Thread.new do
+      scheduler = Evt::Scheduler.new
+      Fiber.set_scheduler scheduler
+
+      Fiber.schedule do
+        3.times do |i|
+          queue << i
+          sleep 0.1
+        end
+
+        queue.close
+      end
+
+      Fiber.schedule do
+        while item = queue.pop
+          processed += 1
+        end
+      end
+
+      scheduler.run
+    end
+
+    thread.join
+
+    assert_equal 3, processed
+  end
+
+  def test_queue_pop_waits
+    queue = Queue.new
+    running = false
+
+    thread = Thread.new do
+      scheduler = Evt::Scheduler.new
+      Fiber.set_scheduler scheduler
+
+      result = nil
+      Fiber.schedule do
+        result = queue.pop
+      end
+
+      running = true
+      scheduler.run
+      result
+    end
+
+    Thread.pass until running
+    sleep 0.1
+
+    queue << :done
+    assert_equal :done, thread.value
+  end
 end
