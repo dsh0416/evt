9f3b6dddbc3f57674ccb3c6c5e4626361e93c92c
Delton Ding <dsh0416@gmail.com>
2020-12-22 20:15:18 +0900

    Add UNIX and TCP tests

---
+++ test/tcp_test.rb
@@ -0,0 +1,81 @@
+require "test_helper"
+require "socket"
+
+class TestTCP < Minitest::Test
+  MESSAGE = "Hello World"
+  BATCH_SIZE = 16
+  PORT = 8765
+
+  def test_read
+    server = TCPServer.new PORT
+    client = TCPSocket.new "127.0.0.1", PORT
+    scheduler = Evt::Scheduler.new
+
+    message = nil
+    thread = Thread.new do
+      Fiber.set_scheduler scheduler
+
+      Fiber.schedule do
+        client.print(MESSAGE)
+        client.close
+      end
+      
+      Fiber.schedule do
+        c = server.accept
+        message = c.gets
+        c.close
+        server.close
+      end
+
+      scheduler.run
+    end
+
+    thread.join
+
+    assert_equal MESSAGE, message
+    assert server.closed?
+    assert client.closed?
+  end
+
+  def test_batch_read
+    server = TCPServer.new PORT
+    clients = BATCH_SIZE.times.map do
+      client = TCPSocket.new "127.0.0.1", PORT
+    end
+    results = []
+    scheduler = Evt::Scheduler.new
+
+    thread = Thread.new do
+      Fiber.set_scheduler scheduler
+
+      clients.each do |client|
+        Fiber.schedule do
+          client.print(MESSAGE)
+          client.close
+        end
+      end
+
+      Fiber.schedule do
+        BATCH_SIZE.times do
+          c = server.accept
+          results << c.gets
+          c.close
+        end
+        server.close
+      end
+
+      scheduler.run
+    end
+    
+    thread.join
+
+    assert_equal BATCH_SIZE, results.length
+    results.each do |message|
+      assert_equal MESSAGE, message
+    end
+    assert server.closed?
+    clients.each do |client|
+      assert client.closed?
+    end
+  end
+end
---
+++ test/unix_test.rb
@@ -0,0 +1,73 @@
+require "test_helper"
+
+class TestUNIX < Minitest::Test
+  MESSAGE = "Hello World"
+  BATCH_SIZE = 16
+
+  def test_read
+    raise Minitest::Skip, "Windows doesn't have UNIXSocket, skipping test", caller if Gem.win_platform?
+    rd, wr = UNIXSocket.pair
+    scheduler = Evt::Scheduler.new
+
+    message = nil
+    thread = Thread.new do
+      Fiber.set_scheduler scheduler
+
+      Fiber.schedule do
+        wr.write(MESSAGE)
+        wr.close
+      end
+      
+      Fiber.schedule do
+        message = rd.read(20)
+        rd.close
+      end
+
+      scheduler.run
+    end
+
+    thread.join
+
+    assert_equal MESSAGE, message
+    assert rd.closed?
+    assert wr.closed?
+  end
+
+  def test_batch_read
+    raise Minitest::Skip, "Windows doesn't have UNIXSocket, skipping test", caller if Gem.win_platform?
+    ios = BATCH_SIZE.times.map { UNIXSocket.pair }
+    results = []
+    scheduler = Evt::Scheduler.new
+
+    thread = Thread.new do
+      Fiber.set_scheduler scheduler
+
+      ios.each do |io|
+        rd, wr = io
+        Fiber.schedule do
+          results << rd.read(20)
+          rd.close
+        end
+  
+        Fiber.schedule do
+          wr.write(MESSAGE)
+          wr.close
+        end
+      end
+
+      scheduler.run
+    end
+    
+    thread.join
+
+    assert_equal BATCH_SIZE, results.length
+    results.each do |message|
+      assert_equal MESSAGE, message
+    end
+    ios.each do |io|
+      rd, wr = io
+      assert rd.closed?
+      assert wr.closed?
+    end
+  end
+end
