a63527212cb434a92c0bd6c5e3c9847278771e21
Delton Ding <dsh0416@gmail.com>
2020-12-22 12:04:08 +0900

    Fix tests

--- ext/evt/extconf.rb
+++ ext/evt/extconf.rb
@@ -2,14 +2,22 @@ require 'mkmf'
 extension_name = 'evt_ext'
 dir_config(extension_name)
 
-unless ENV['DISABLE_URING']
+def env_defined?(v)
+  return false if ENV[v].nil?
+  return false if ENV[v].empty?
+  return false if ENV[v].upcase == 'FALSE'
+  return false if ENV[v] == '0'
+  true
+end
+
+unless env_defined?('DISABLE_URING')
   have_library('uring')
   have_header('liburing.h')
 end
 
-have_header('sys/epoll.h') unless ENV['DISABLE_EPOLL']
-have_header('sys/event.h') unless ENV['DISABLE_KQUEUE']
-have_header('Windows.h')  unless ENV['DISABLE_IOCP']
+have_header('sys/epoll.h') unless env_defined?('DISABLE_EPOLL')
+have_header('sys/event.h') unless env_defined?('DISABLE_KQUEUE')
+have_header('Windows.h')  unless env_defined?('DISABLE_IOCP')
 
 create_header
 create_makefile(extension_name)
