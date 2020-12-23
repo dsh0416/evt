78352ec9214eb60b649e0c2ae0d560245b7a2d81
Delton Ding <dsh0416@gmail.com>
2020-12-23 16:59:13 +0900

    Fix select

--- ext/evt/select.h
+++ ext/evt/select.h
@@ -6,7 +6,8 @@ VALUE method_scheduler_select_wait(VALUE self) {
     // return IO.select(@readable.keys, @writable.keys, [], next_timeout / 1000.0)
     ID id_select = rb_intern("select");
     ID id_next_timeout = rb_intern("next_timeout");
-    ID id_div = rb_intern("div");
+    ID id_div = rb_intern("/");
+    ID id_to_f = rb_intern("to_f");
 
     VALUE readable = rb_iv_get(self, "@readable");
     VALUE writable = rb_iv_get(self, "@writable");
@@ -14,7 +15,8 @@ VALUE method_scheduler_select_wait(VALUE self) {
     VALUE readable_keys = rb_funcall(readable, rb_intern("keys"), 0);
     VALUE writable_keys = rb_funcall(writable, rb_intern("keys"), 0);
     VALUE next_timeout = rb_funcall(self, id_next_timeout, 0);
-    VALUE secs = rb_funcall(next_timeout, id_div, DBL2NUM(1000.0));
+    next_timeout = rb_funcall(next_timeout, id_to_f, 0);
+    VALUE secs = rb_funcall(next_timeout, id_div, 1, DBL2NUM(1000.0));
 
     return rb_funcall(rb_cIO, id_select, 4, readable_keys, writable_keys, rb_ary_new(), secs);
 }
