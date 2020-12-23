c1117c4c0dbe8da8020b3b776e5169984cdbe253
Delton Ding <dsh0416@gmail.com>
2020-12-23 16:55:35 +0900

    Fix select timeout

--- ext/evt/select.h
+++ ext/evt/select.h
@@ -3,18 +3,17 @@
 #include "evt.h"
 
 VALUE method_scheduler_select_wait(VALUE self) {
-    // return IO.select(@readable.keys, @writable.keys, [], next_timeout)
-    VALUE readable, writable, readable_keys, writable_keys, next_timeout;
+    // return IO.select(@readable.keys, @writable.keys, [], next_timeout / 1000.0)
     ID id_select = rb_intern("select");
     ID id_next_timeout = rb_intern("next_timeout");
     ID id_div = rb_intern("div");
 
-    readable = rb_iv_get(self, "@readable");
-    writable = rb_iv_get(self, "@writable");
+    VALUE readable = rb_iv_get(self, "@readable");
+    VALUE writable = rb_iv_get(self, "@writable");
 
-    readable_keys = rb_funcall(readable, rb_intern("keys"), 0);
-    writable_keys = rb_funcall(writable, rb_intern("keys"), 0);
-    next_timeout = rb_funcall(self, id_next_timeout, 0);
+    VALUE readable_keys = rb_funcall(readable, rb_intern("keys"), 0);
+    VALUE writable_keys = rb_funcall(writable, rb_intern("keys"), 0);
+    VALUE next_timeout = rb_funcall(self, id_next_timeout, 0);
     VALUE secs = rb_funcall(next_timeout, id_div, DBL2NUM(1000.0));
 
     return rb_funcall(rb_cIO, id_select, 4, readable_keys, writable_keys, rb_ary_new(), secs);
